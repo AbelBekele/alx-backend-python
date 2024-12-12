77c658591bf773c4d7dcde672e8f7e2b1e2ba68e
Abel Bekele <abelbekele.addise@gmail.com>
2024-12-12 23:38:50 +0300

    updating memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -39,18 +39,21 @@ class TestGetJson(unittest.TestCase):
         self.assertEqual(result, test_payload)
 
 class TestMemoize(unittest.TestCase):
+    """Test cases for memoize decorator"""
+
     def test_memoize(self):
+        """Test memoize decorator works as expected"""
         class TestClass:
             def a_method(self):
                 return 42
 
-            @property
-            @patch('utils.memoize', side_effect=lambda f: f)  
+            @memoize
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_a_method:
-            obj = TestClass()
-            self.assertEqual(obj.a_property, 42)
-            self.assertEqual(obj.a_property, 42)
-            mock_a_method.assert_called_once()
+        with patch.object(TestClass, 'a_method',
+                         return_value=42) as mock_method:
+            test_obj = TestClass()
+            self.assertEqual(test_obj.a_property, 42)
+            self.assertEqual(test_obj.a_property, 42)
+            mock_method.assert_called_once()
