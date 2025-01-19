8ea73810fbc8f4e6c91bfe1ec28d4c7ab62a5107
Abel Bekele <abelbekele.addise@gmail.com>
2025-01-19 21:53:22 +0300

    cnaging name to Django-Middleware-0x03

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -75,7 +75,7 @@ MIDDLEWARE = [
 # Custom user model
 AUTH_USER_MODEL = 'chats.User'
 
-ROOT_URLCONF = 'messaging_app.urls'
+ROOT_URLCONF = 'Django-Middleware-0x03.urls'
 
 TEMPLATES = [
     {
@@ -93,7 +93,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'messaging_app.wsgi.application'
+WSGI_APPLICATION = 'Django-Middleware-0x03.wsgi.application'
 
 # JWT Settings
 
